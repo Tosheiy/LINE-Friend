da44fbc0df941a7e22ee1b835b7e8f1e36bcde22
toshi <100521386+ErrwinSmith@users.noreply.github.com>
2023-07-08 20:18:17 +0900

    ChatGPTの返答機能の実装

--- src/event/message/text.js
+++ src/event/message/text.js
@@ -1,6 +1,8 @@
+import { Configuration, OpenAIApi } from "openai";
 import { contextManage } from './context-manage.js';
 import { hasKey } from '../../haskey.js';
 import { messageMap } from './text-map.js';
+
 
 // テキストメッセージの処理をする関数
 export const textEvent = async (event, appContext) => {
@@ -18,8 +20,23 @@ export const textEvent = async (event, appContext) => {
   }
 
   // 返信するメッセージが存在しない場合
+  const configuration = new Configuration({
+    organization: "org-3VqlCichKGO9XBDB6miCm6Ar",
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createChatCompletion({
+    model: "gpt-4-0613",
+    messages: [{ "role": "system", "content": "あなたは私の彼女です。ギャルのように会話してください" }, { role: "user", content: `${receivedMessage}` }],
+  });
+  console.log(completion.data.choices[0].message);
+
+  const gptResponse1 = JSON.stringify(completion.data.choices[0].message.content);
+
+
   return {
     type: 'text',
-    text: `受け取ったメッセージ: ${receivedMessage}\nそのメッセージの返信には対応してません...`,
+    text: `${gptResponse1}`,
   };
 };
+
