0f5d0a186b50d99344080790f71b96daa3117cde
Renoruiga <137611555+Renoruiga@users.noreply.github.com>
2023-07-15 21:25:03 +0900

    定期メッセージの実装(ChatGPT)

--- src/event/message/text.js
+++ src/event/message/text.js
@@ -1,3 +1,4 @@
+import { Configuration, OpenAIApi } from 'openai';
 import { contextManage } from './context-manage.js';
 import { hasKey } from '../../haskey.js';
 import { messageMap } from './text-map.js';
@@ -18,8 +19,20 @@ export const textEvent = async (event, appContext) => {
   }
 
   // 返信するメッセージが存在しない場合
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'system', content: 'あなたは私の彼女です。ギャルのように会話してください' }, { role: 'user', content: `${receivedMessage}` }],
+  });
+  console.log(completion.data.choices[0].message);
+
+  const gptResponse1 = completion.data.choices[0].message.content;
+
   return {
     type: 'text',
-    text: `受け取ったメッセージ: ${receivedMessage}\nそのメッセージの返信には対応してません...`,
+    text: `${gptResponse1}`,
   };
 };
